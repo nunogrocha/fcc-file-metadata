d429dc192fb4924019f4efdb3c7fc46fd6dbb013
Nuno Rocha <nunogcrocha@outlook.com>
2016-06-07 10:53:55 +0100

    0.1

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
-router.get('/fileanalyse', function(req, res, next) {
-  
+router.post('/fileanalyse', upload.single('file'), function(req, res, next) {
+  console.log(req.file)
+  res.json({size:req.file.size});
 });
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FreeCodeCamp Challenge: URL Shortener Microservice' });
+  res.render('index', { title: 'FreeCodeCamp Challenge: File Metadata Microservice' });
 });
 
 module.exports = router;
