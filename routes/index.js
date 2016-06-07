d47ca64e64851d40f3ba6912b00611981d285522
Nuno Rocha <nunogcrocha@outlook.com>
2016-06-07 11:02:56 +0100

    fixed ajax type

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var upload = multer()
 
 router.post('/fileanalyse', upload.single('file'), function(req, res, next) {
   console.log(req.file)
